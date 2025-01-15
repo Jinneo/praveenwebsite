0dfcbe7c9e34ebf1acd8fd36cc155e7fea647714
Praveen Vadlamani <pvadl352@gmail.com>
2025-01-15 02:10:39 -0800

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router";
-import { Header } from "./Header";
-import { Footer } from "./Footer";
-import { Home } from "./Home";
-import { Portfolio } from "./Portfolio";
 import { Box } from "@chakra-ui/react";
-import { NotFound } from "./NotFound";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Home"; // Ensure correct imports for your components
+import Portfolio from "./Portfolio";
+import NotFound from "./NotFound";
+import Header from "./Header";
+import Footer from "./Footer";
 
 const router = createBrowserRouter([
   { path: "/praveenwebsite/", element: <Home /> },
   { path: "/praveenwebsite/portfolio", element: <Portfolio /> },
-  {path: "*", element: <NotFound />}
+  { path: "/praveenwebsite/*", element: <NotFound /> }, // Catch-all route for unmatched URLs
 ]);
 
 function App() {
   return (
-    <Box height="" display="flex" flexDirection="column">
+    <Box display="flex" flexDirection="column" minHeight="100vh">
       <Header />
       <RouterProvider router={router} />
       <Footer />
